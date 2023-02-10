d5952abbfca69b90c9924828bcf49f080d4997f3
Raghav Aggarwal(UST <164112@ust-global.com>
2023-02-10 14:56:47 +0530

    Redirecting from login to profile page

--- Nodejs/Login.js
+++ Nodejs/Login.js
@@ -1,24 +1,25 @@
-
-
 function Validate() {
   var username = document.getElementById("username");
   var password = document.getElementById("password");
-  var label = document.getElementById("error-label")
-  var label1 = document.getElementById("error-label1")
-
+  var label = document.getElementById("error-label");
+  var label1 = document.getElementById("error-label1");
 
   if (username.value.trim() == "") {
     username.style.border = "3px solid red";
-    label.innerHTML="Username cannot be empty"
-   // window.location.reload();
+    label.innerHTML = "Username cannot be empty";
+    // window.location.reload();
     return false;
   } else if (password.value.trim() == "") {
     password.style.border = "3px solid red";
-    label1.innerHTML="Password cannot be empty"
+    label1.innerHTML = "Password cannot be empty";
     return false;
-  } else {
+  } else if (
+    username.value == localStorage.getItem("username") &&
+    password.value == localStorage.getItem("password")
+  ) {
     return true;
+  } else {
+    alert("Please enter correct values");
+    return false;
   }
-  
 }
-
--- Nodejs/Registeration.js
+++ Nodejs/Registeration.js
@@ -1,27 +1,28 @@
 function Validate() {
   var password = document.getElementById("password");
   var confirmPassword = document.getElementById("cpassword");
-  let fname = document.getElementById("fname").value
-  let lname = document.getElementById("lname").value
-  let dob = document.getElementById("dob").value
-  let age = document.getElementById("age").value
-  
+  let fname = document.getElementById("fname").value;
+  let lname = document.getElementById("lname").value;
+  let dob = document.getElementById("dob").value;
+  let age = document.getElementById("age").value;
+  let username = document.getElementById("username").value;
+
   if (password.value.trim() == "") {
     password.style.border = "3px solid red";
-    return false
+    return false;
   } else if (confirmPassword.value.trim() == "") {
     cpassword.style.border = "3px solid red";
-    return false
+    return false;
   } else if (password.value != confirmPassword.value) {
     alert("Passwords do not match.");
-    return false
+    return false;
   } else {
     localStorage.setItem("fname", fname);
     localStorage.setItem("lname", lname);
     localStorage.setItem("dob", dob);
     localStorage.setItem("age", age);
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password.value);
     return true;
   }
 }
-
-  
